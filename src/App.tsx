0caaeb483bcaa5ec0a4e7de61b368baabc8ee93d
Nathan Stabille <nathan.stabille@hotmail.com>
2022-08-04 20:39:12 -0300

    localStorage add

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import * as C from "./App.Style";
 import { InputItem } from "./components/InputItem";
 import { ListItem } from "./components/ListItem";
 import { ListType } from "./types/ListType";
 
+const SAVED_ITEMS = "savedItems";
+
 export const App = () => {
-  const [list, setList] = useState<ListType[]>([]);
+  const getList = () => {
+    const savedItems = localStorage.getItem(SAVED_ITEMS);
+    if (!savedItems) {
+      return [];
+    }
+    return JSON.parse(savedItems);
+  };
+
+  const [list, setList] = useState<ListType[]>(getList());
+
+  useEffect(() => {
+    localStorage.setItem(SAVED_ITEMS, JSON.stringify(list));
+  }, [list]);
 
   return (
     <C.Container>
